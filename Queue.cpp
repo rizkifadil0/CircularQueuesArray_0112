614d3a888056be1008bbc5f503e756db00e26959
rixkifadil@gmail.com <rixkifadil@gmail.com>
2025-06-12 15:01:16 +0700

    add mengecek antrian (penuh/belum penuh)

--- Queue.cpp
+++ Queue.cpp
@@ -14,5 +14,38 @@ class Queues{
             REAR = -1;
         }
 
+        void insert()
+        {
+            int num;
+            cout << "Enter a number : ";
+            cin >> num;
+            cout << endl;
+
+            //1. Cek apakah antrian penuh
+            if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1))
+            {
+                cout << "\nQueue overflow\n"; // 1.a
+                return ; 
+            }
+            if (FRONT == -1)
+            {
+                FRONT = 0; // 2.a
+                REAR = 0; //2.b
+
+            }
+            else
+            {
+                //jika benar berada dalam posisi terkhir aarray, kembali ke titik awal array
+                if (REAR == max - 1)
+                    REAR = 0;
+                else
+                    REAR = REAR + 1;
+            }
+            queue_array[REAR] = num;
+        }
+
         
+
+
 };
+
